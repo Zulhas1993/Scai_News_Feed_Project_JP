9d2a983ee7c235fc1361c76303aaf3fe46f8de3c
DESKTOP-361QN81\admin <mdzulhasuddin95@gmail.com>
2024-01-22 16:58:10 +0600

    modify news details

--- component/Extract_News_Feed.py
+++ component/Extract_News_Feed.py
@@ -10,8 +10,11 @@ def read_json(file_path):
         data = json.load(file)
     return data
 
+
 def extract_news_info(data):
     news_list = []
+    unique_links = set()  # Use a set to track unique links
+
     for key, value in data.items():
         for entry in value.values():
             entry_data = json.loads(entry)
@@ -19,8 +22,36 @@ def extract_news_info(data):
             link = entry_data.get('link', '')
             description = entry_data.get('description', '')
             subjectList = entry_data.get('subjectList', [])
-            news_list.append({'title': title, 'link': link, 'description': description, 'subjectList': subjectList})
+
+            # Check if the link is not a duplicate and has details
+            if link not in unique_links and description:
+                news_list.append({'title': title, 'link': link, 'description': description, 'subjectList': subjectList})
+                unique_links.add(link)
+
     return news_list
+
+
+
+
+# def get_news_details(link):
+#     try:
+#         # You can customize this function to fetch news details from the provided link
+#         response = requests.get(link)
+#         response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+        
+#         soup = BeautifulSoup(response.text, 'html.parser')        
+#         # Extract text content from the webpage
+#         details_text = soup.get_text()
+#         max_document_size = 5120  # Maximum allowed text elements
+#         if len(details_text) > max_document_size:
+#             details_text = details_text[:max_document_size]
+            
+#         # Return the news details
+#         return {'content': details_text}
+#     except requests.RequestException as e:
+#         raise FetchDetailsError(f"Error fetching details for link: {link}. {str(e)}")
+
+
 
 def get_news_details(link):
     try:
@@ -31,14 +62,17 @@ def get_news_details(link):
         soup = BeautifulSoup(response.text, 'html.parser')        
         # Extract text content from the webpage
         details_text = soup.get_text()
-        max_document_size = 5120  # Maximum allowed text elements
-        if len(details_text) > max_document_size:
-            details_text = details_text[:max_document_size]
-            
-        # Return the news details
-        return {'content': details_text}
+
+        # Check if the details_text is not empty or contains only whitespace
+        if details_text and not details_text.isspace():
+            # Return the news details
+            return {'content': details_text}
+        else:
+            # Return None if there are no details
+            return None
     except requests.RequestException as e:
         raise FetchDetailsError(f"Error fetching details for link: {link}. {str(e)}")
+
 
 def get_unique_links_with_count(news_list):
     links_count = {}  # Use a dictionary to store unique links and their counts
